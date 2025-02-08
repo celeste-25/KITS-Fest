d6e4ec7f81511210d3854304bf094c429a0d7fa0
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-08 16:08:25 +0530

    Update script.js

--- tech-fest/script.js
+++ tech-fest/script.js
@@ -4,6 +4,7 @@ const card = document.getElementById("card");
 
 let registrationUrl = "";
 
+// Dynamically setting registration URL based on the page
 if (window.location.pathname.includes('th.html')) {
     registrationUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfX8ovXyEv-CLf3BF6oXscR4zqYlV3Fu_mUHzD66BC_yHIqmg/viewform?usp=header";
 } else if (window.location.pathname.includes('toss-the-egg.html')) {
@@ -14,14 +15,17 @@ if (window.location.pathname.includes('th.html')) {
     registrationUrl = "https://default-link.com";
 }
 
+// Enable register button only when checkbox is checked
 checkbox.addEventListener("change", function () {
     registerButton.disabled = !this.checked;
 });
 
+// Redirect on register button click
 registerButton.addEventListener("click", function () {
     window.location.href = registrationUrl;
 });
 
+// Flip Card Function
 function flipCard() {
     card.classList.toggle('flipped');
 }
