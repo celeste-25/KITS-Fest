1ceac06e172a4464fe2c466180dde4c2eeaa3d4d
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-08 15:27:01 +0530

    Update script.js

--- tech-fest/script.js
+++ tech-fest/script.js
@@ -1,26 +1,31 @@
 const checkbox = document.getElementById("agree-checkbox");
 const registerButton = document.getElementById("register-button");
+const card = document.getElementById("card");
 
 let registrationUrl = "";
 
+// Dynamically setting registration URL based on the page
 if (window.location.pathname.includes('th.html')) {
-  registrationUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfX8ovXyEv-CLf3BF6oXscR4zqYlV3Fu_mUHzD66BC_yHIqmg/viewform?usp=header";
-} else if (window.location.pathname.includes('bgmi.html')) {
-registrationUrl = "https://celeste-25.github.io/KITS-Fest/payment/payment.html";
+    registrationUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfX8ovXyEv-CLf3BF6oXscR4zqYlV3Fu_mUHzD66BC_yHIqmg/viewform?usp=header";
+} else if (window.location.pathname.includes('tg2.html')) {
+    registrationUrl = "https://celeste-25.github.io/KITS-Fest/payment/payment.html";
 } else if (window.location.pathname.includes('page2.html')) {
-  registrationUrl = "https://link-for-page2.com";
+    registrationUrl = "https://link-for-page2.com";
 } else {
-  registrationUrl = "https://default-link.com";
+    registrationUrl = "https://default-link.com";
 }
 
+// Enable register button only when checkbox is checked
 checkbox.addEventListener("change", function () {
-  if (this.checked) {
-    registerButton.disabled = false;
-  } else {
-    registerButton.disabled = true;
-  }
+    registerButton.disabled = !this.checked;
 });
 
+// Redirect on register button click
 registerButton.addEventListener("click", function () {
-  window.location.href = registrationUrl;
+    window.location.href = registrationUrl;
 });
+
+// Flip Card Function
+function flipCard() {
+    card.classList.toggle('flipped');
+}
