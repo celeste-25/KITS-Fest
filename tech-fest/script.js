5e17e4afcb0f40f289d9c3b2fa28eed33ca570aa
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-08 15:30:48 +0530

    Update script.js

--- tech-fest/script.js
+++ tech-fest/script.js
@@ -7,7 +7,7 @@ let registrationUrl = "";
 // Dynamically setting registration URL based on the page
 if (window.location.pathname.includes('th.html')) {
     registrationUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfX8ovXyEv-CLf3BF6oXscR4zqYlV3Fu_mUHzD66BC_yHIqmg/viewform?usp=header";
-} else if (window.location.pathname.includes('tg2.html')) {
+} else if (window.location.pathname.includes('toss-the-egg.html')) {
     registrationUrl = "https://celeste-25.github.io/KITS-Fest/payment/payment.html";
 } else if (window.location.pathname.includes('page2.html')) {
     registrationUrl = "https://link-for-page2.com";
