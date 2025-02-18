57eb02719b9e5aa6e5e1c17c2407a0c6872e89aa
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-18 15:03:32 +0530

    Update spot-choreo-reg.js

--- payment/arts-fest/spot-choreo-reg.js
+++ payment/arts-fest/spot-choreo-reg.js
@@ -1,4 +1,4 @@
-const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbytUKT_BDzwhlc-EJ0Cr5QaUwg9PaHGyyHhYv_kOqOqWIZ2UPf-7pbTbBU5f2oOxWyo/exec'; 
+const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbyFOlcsY27NFfmIOZKxJ26JJzOp1JpXJ3hb_bGDWqFZmvtygcSdtQ7kSBg4XVOhO8WK/exec'; 
 const offlineForm = document.forms['offlinePaymentForm'];
 const eventFieldOffline = document.getElementById("event");
 
