7e65345c765801819e5326d6e746993a55ca3c3a
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-18 20:31:52 +0530

    Create grp-dance-reg.js

---
+++ payment/arts-fest/grp-dance-reg.js
@@ -0,0 +1,26 @@
+const groupScriptURL = 'https://script.google.com/macros/s/AKfycby5k60kwty9IBrU1chq8iHfgBsEj9CjyO_ei6WB2GSTHMzFKq-dIcRLDIxdVmeSItRvNw/exec'; 
+const groupForm = document.forms['groupEventForm'];
+const eventFieldGroup = document.getElementById("event");
+
+const urlParamsGroup = new URLSearchParams(window.location.search);
+const eventNameGroup = urlParamsGroup.get("event");
+if (eventNameGroup) {
+    eventFieldGroup.value = eventNameGroup;
+}
+
+groupForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(groupForm);
+
+    fetch(groupScriptURL, { 
+        method: 'POST', 
+        body: formData, 
+        mode: "no-cors" 
+    })
+    .then(() => {
+        alert("Thank you! Your registration details are submitted.");
+        groupForm.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
