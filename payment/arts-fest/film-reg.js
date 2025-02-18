1425c26516e4e01c56d0a18a53fa81cb2da844c5
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-18 21:02:25 +0530

    Create film-reg.js

---
+++ payment/arts-fest/film-reg.js
@@ -0,0 +1,26 @@
+const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbzTP2_tPaK9Eifv0_MSVIJEPaox6yAfzEBN8GQxlUiTkPMHSqIR3QQRl9T0r0Mby1zorg/exec'; 
+const offlineForm = document.forms['offlinePaymentForm'];
+const eventFieldOffline = document.getElementById("event");
+
+const urlParamsOffline = new URLSearchParams(window.location.search);
+const eventNameOffline = urlParamsOffline.get("event");
+if (eventNameOffline) {
+    eventFieldOffline.value = eventNameOffline;
+}
+
+offlineForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(offlineForm);
+
+    fetch(offlineScriptURL, { 
+        method: 'POST', 
+        body: formData, 
+        mode: "no-cors" 
+    })
+    .then(() => {
+        alert("Thank you! Your offline payment details are submitted.");
+        offlineForm.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
