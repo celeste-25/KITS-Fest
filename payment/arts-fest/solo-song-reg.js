229c4d2bb137084a9a688f61eaf313e6f5552a43
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-18 12:08:48 +0530

    Create solo-song-reg.js

---
+++ payment/arts-fest/solo-song-reg.js
@@ -0,0 +1,28 @@
+const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbzLkV_RZzsrVjJbBQ8bX3XKxb4lE6Paa0nhgB4B4cVWDMh4-6uPjNQS7jtRaRyuY9c8/exec'; 
+const offlineForm = document.forms['offlinePaymentForm'];
+const eventFieldOffline = document.getElementById("event");
+
+const urlParamsOffline = new URLSearchParams(window.location.search);
+const eventNameOffline = urlParamsOffline.get("event");
+if (eventNameOffline) {
+    eventFieldOffline.value = eventNameOffline;
+}
+
+offlineForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", offlineForm.name.value);
+    formData.append("email", offlineForm.email.value);
+    formData.append("year", offlineForm.year.value);
+    formData.append("branch", offlineForm.branch.value);
+    formData.append("event", eventFieldOffline.value); 
+    formData.append("registrationNumber", offlineForm.registrationNumber.value);
+
+    fetch(offlineScriptURL, { method: 'POST', body: formData, mode: "no-cors" })
+    .then(() => {
+        alert("Thank you! Your offline payment details are submitted.");
+        offlineForm.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
