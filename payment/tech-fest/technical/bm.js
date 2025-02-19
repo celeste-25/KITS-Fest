bd01b7f62e4d3251fb04baa2fdbf6e8cdebbf5a3
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-19 12:31:12 +0530

    Update bm.js

--- payment/tech-fest/technical/bm.js
+++ payment/tech-fest/technical/bm.js
@@ -1,4 +1,4 @@
-const groupScriptURL = 'https://script.google.com/macros/s/AKfycbzYSmrSDQOBaw8u3Hip20EXaxHeEGZ7DtJlQusKykWcXho5FJ-sEKsOftWBl-Jkniqr/exec'; 
+const groupScriptURL = 'https://script.google.com/macros/s/AKfycbwwrmQYlcyDICKNafdRGkDn5R64eWoKwlt8uTkrpCn0GT1_zIGAuidS-zIymQgc7_q7/exec'; 
 const groupForm = document.forms['groupEventForm'];
 const eventFieldGroup = document.getElementById("event");
 
