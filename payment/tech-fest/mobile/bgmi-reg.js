c3c47fb3296dafdfb80d5144ab33ff3fca1f6ff9
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-19 21:53:56 +0530

    Create bgmi-reg.js

---
+++ payment/tech-fest/mobile/bgmi-reg.js
@@ -0,0 +1,22 @@
+const bgmiForm = document.forms['groupEventForm'];
+bgmiForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(bgmiForm);
+  
+  let fr = new FileReader();
+  fr.onloadend = () => {
+    let base64Data = fr.result.split("base64,")[1];
+    formData.append("base64", base64Data);
+    formData.append("imageType", fileInput.files[0].type);
+    formData.append("imageName", fileInput.files[0].name);
+    
+    fetch(bgmiForm.action, { method: 'POST', body: formData, mode: "no-cors" })
+      .then(() => {
+        alert("Thank you! Your BGMI registration details are submitted.");
+        bgmiForm.reset();
+        preview.style.display = "none";
+      })
+      .catch(error => console.error('Error!', error.message));
+  };
+  fr.readAsDataURL(fileInput.files[0]);
+});
