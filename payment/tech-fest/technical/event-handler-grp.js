e0983dd3aacbd4ca3f52734e4bbbabdf20fcc547
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-19 11:25:10 +0530

    Create event-handler-grp.js

---
+++ payment/tech-fest/technical/event-handler-grp.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const eventName = urlParams.get("event").toUpperCase().replace(/%20/g, " ");
+    const eventField = document.getElementById("event");
+    
+    if (eventName) {
+        eventField.value = eventName;
+        let scriptSrc;
+
+        switch (eventName) {
+            case "TREASURE HUNT":
+                scriptSrc = "th.js";
+                break;
+            case "BRIDGE MAKING":
+                scriptSrc = "bridge-making.js";
+                break;
+            case "TOSS THE EGG":
+                scriptSrc = "toss-the-egg.js";
+                break;
+            case "LOGO IDENTIFICATION":
+                scriptSrc = "logo-identification.js";
+                break;
+            default:
+                scriptSrc = "";
+        }
+
+        if (scriptSrc) {
+            const existingScript = document.getElementById('eventScript');
+            if (existingScript) {
+                existingScript.remove();
+            }
+            
+            const script = document.createElement('script');
+            script.id = 'eventScript';
+            script.src = scriptSrc;
+            document.body.appendChild(script);
+        }
+    }
+});
