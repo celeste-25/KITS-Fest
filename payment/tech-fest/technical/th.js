046dd55ba3222d49c8c4d9e4e7447b3eb1abf32e
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-19 11:36:11 +0530

    Create th.js

---
+++ payment/tech-fest/technical/th.js
@@ -0,0 +1,26 @@
+const groupScriptURL = 'https://script.google.com/macros/s/AKfycby3Qu1APeRzR4-9BR-QPJ4-mFmFQrpAfAieXI3Ma-IOocI-AOmXbSXC6Ez1hENWSIXl/exec'; 
+const groupForm = document.forms['groupEventForm'];
+const eventFieldGroup = document.getElementById("event");
+
+const urlParamsGroup = new URLSearchParams(window.location.search);
+const eventNameGroup = urlParamsGroup.get("event");
+if (eventNameGroup) {
+    eventFieldGroup.value = eventNameGroup;
+}
+
+groupForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(groupForm);
+
+    fetch(groupScriptURL, { 
+        method: 'POST', 
+        body: formData, 
+        mode: "no-cors" 
+    })
+    .then(() => {
+        alert("Thank you! Your registration details are submitted.");
+        groupForm.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
