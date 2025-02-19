e499d13dee97132dab128dbc4f91c136b57c5f84
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-19 23:00:26 +0530

    Update ff-reg.js

--- payment/tech-fest/mobile/ff-reg.js
+++ payment/tech-fest/mobile/ff-reg.js
@@ -3,14 +3,14 @@ const bgmiForm = document.forms['groupEventForm'];
 bgmiForm.addEventListener('submit', e => {
   e.preventDefault();
   const formData = new FormData(bgmiForm);
-  
+
   let fr = new FileReader();
   fr.onloadend = () => {
     let base64Data = fr.result.split("base64,")[1];
     formData.append("base64", base64Data);
     formData.append("imageType", fileInput.files[0].type);
     formData.append("imageName", fileInput.files[0].name);
-    
+
     fetch(bgmiForm.action, { method: 'POST', body: formData })
       .then(response => response.json())
       .then(data => {
