c538cef98d2ebad83657654b1948f5d4c47072c4
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-20 15:28:56 +0530

    Create event-handler.js

---
+++ payment/tech-fest/mobile/event-handler.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const eventName = urlParams.get("event").toUpperCase().replace(/%20/g, " ");
+    const eventField = document.getElementById("event");
+    
+    if (eventName) {
+        eventField.value = eventName;
+        let scriptSrc;
+
+        switch (eventName) {
+            case "BGMI":
+                scriptSrc = "bgmi-reg.js";
+                break;
+            case "Free Fire":
+                scriptSrc = "ff-reg.js";
+                break;
+            case "E-Football":
+                scriptSrc = "pes-reg.js";
+                break;
+            default:
+                scriptSrc = "";
+        }
+
+        if (scriptSrc) {
+            const existingScript = document.getElementById('eventScript');
+            if (existingScript) {
+                existingScript.remove();
+            }
+            
+            const script = document.createElement('script');
+            script.id = 'eventScript';
+            script.src = scriptSrc;
+            document.body.appendChild(script);
+        }
+    }
+});
