ff8b26076d65c8b793e59f3668f8ff365f60de3e
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-20 11:30:15 +0530

    Update pes-reg.js

--- payment/tech-fest/mobile/pes-reg.js
+++ payment/tech-fest/mobile/pes-reg.js
@@ -1,22 +1,29 @@
-const pesForm = document.forms['groupEventForm'];
-pesForm.addEventListener('submit', e => {
+const bgmiForm = document.forms['groupEventForm'];
+
+bgmiForm.addEventListener('submit', e => {
   e.preventDefault();
-  const formData = new FormData(pesForm);
-  
+  const formData = new FormData(bgmiForm);
+
   let fr = new FileReader();
   fr.onloadend = () => {
     let base64Data = fr.result.split("base64,")[1];
     formData.append("base64", base64Data);
     formData.append("imageType", fileInput.files[0].type);
     formData.append("imageName", fileInput.files[0].name);
-    
-    fetch(pesForm.action, { method: 'POST', body: formData, mode: "no-cors" })
-      .then(() => {
-        alert("Thank you! Your E-Football registration details are submitted.");
-        pesForm.reset();
-        preview.style.display = "none";
+
+    fetch(bgmiForm.action, { method: 'POST', body: formData })
+      .then(response => response.json())
+      .then(data => {
+        alert(data.message);
+        if (data.result === 'success') {
+          bgmiForm.reset();
+          preview.style.display = "none";
+        }
       })
-      .catch(error => console.error('Error!', error.message));
+      .catch(error => {
+        alert('Error submitting your registration. Please try again later.');
+        console.error('Error!', error.message);
+      });
   };
   fr.readAsDataURL(fileInput.files[0]);
 });
