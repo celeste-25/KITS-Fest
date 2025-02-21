cb56f5da3f56b7763c60348198fb2a813d596f62
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-21 21:45:51 +0530

    Create football.js

---
+++ payment/sports-fest/mens/football.js
@@ -0,0 +1,26 @@
+const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbzkCmSKMGrjUrIl4laqVcN87DgDq-DjdHWEdMKm5Wi--ykHvKFnlJKsSmDNNxoqC3ew/exec'; 
+const offlineForm = document.forms['offlinePaymentForm'];
+const eventFieldOffline = document.getElementById("event");
+
+const urlParamsOffline = new URLSearchParams(window.location.search);
+const eventNameOffline = urlParamsOffline.get("event");
+if (eventNameOffline) {
+    eventFieldOffline.value = eventNameOffline;
+}
+
+offlineForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(offlineForm);
+
+    fetch(offlineScriptURL, { 
+        method: 'POST', 
+        body: formData, 
+        mode: "no-cors" 
+    })
+    .then(() => {
+        alert("Thank you! Your registration details are successfully submitted.");
+        offlineForm.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
