70f10969582618ff6a0435f978a7530b44c2f469
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-21 18:49:00 +0530

    Update 100m.js

--- payment/sports-fest/athletics/100m.js
+++ payment/sports-fest/athletics/100m.js
@@ -1,4 +1,4 @@
-const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbz-s6PIJMMYNFYDzs7zK0F6KJmSJtkGIsIkARPQwlYmteN-ANSLGCLJ7BHkBBDLcAaDYg/exec'; 
+const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbwCOVaQ8rSkwJgo2ftHlYOLzKdXzxhcW66z7ONm_YKACZjWeKuE212Usqsx4c4RICJJWQ/exec'; 
 const offlineForm = document.forms['offlinePaymentForm'];
 const eventFieldOffline = document.getElementById("event");
 
@@ -19,7 +19,7 @@ offlineForm.addEventListener('submit', e => {
         mode: "no-cors" 
     })
     .then(() => {
-        alert("Thank you! Your offline payment details are submitted.");
+        alert("Thank you! Your registration details are successfully submitted.");
         offlineForm.reset();
     })
     .catch(error => console.error('Error!', error.message));
