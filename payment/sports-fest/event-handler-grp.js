ed1e1e4195ef61b6c07b1c2f22c2903ce355bee7
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-21 19:20:23 +0530

    Create event-handler-grp.js

---
+++ payment/sports-fest/event-handler-grp.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const eventName = urlParams.get("event").toUpperCase().replace(/%20/g, " ");
+    const eventField = document.getElementById("event");
+    
+    if (eventName) {
+        eventField.value = eventName;
+        let scriptSrc;
+
+        switch (eventName) {
+            case "Mens - Football":
+                scriptSrc = "mens/football.js";
+                break;
+            case "Mens - Cricket":
+                scriptSrc = "mens/cricket.js";
+                break;
+            case "Mens - Volleyball":
+                scriptSrc = "mens/volleyball.js";
+                break;
+            case "Men's - BADMINTON DOUBLES":
+                scriptSrc = "mens/badminton.js";
+                break;
+            case "Mens - Relay 400 Meters":
+                scriptSrc = "mens/relay.js";
+                break;
+            default:
+                scriptSrc = "";
+        }
+
+        if (scriptSrc) {
+            const existingScript = document.getElementById('eventScript');
+            if (existingScript) {
+                existingScript.remove();
+            }
+            
+            const script = document.createElement('script');
+            script.id = 'eventScript';
+            script.src = scriptSrc;
+            document.body.appendChild(script);
+        }
+    }
+});
