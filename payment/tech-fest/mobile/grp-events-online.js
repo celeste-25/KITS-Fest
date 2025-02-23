f404f644596c75c8fb21ffd5abf5c398ad8a0aa1
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-23 13:56:01 +0530

    Update grp-events-online.js

--- payment/tech-fest/mobile/grp-events-online.js
+++ payment/tech-fest/mobile/grp-events-online.js
@@ -6,7 +6,7 @@ const eventFieldGroup = document.getElementById("event");
 const urlParamsGroup = new URLSearchParams(window.location.search);
 const eventNameGroup = urlParamsGroup.get("event");
 if (eventNameGroup) {
-    eventFieldGroup.value = decodeURIComponent(eventNameGroup); 
+    eventFieldGroup.value = decodeURIComponent(eventNameGroup);
 }
 
 fileInput.addEventListener("change", () => {
@@ -21,26 +21,20 @@ fileInput.addEventListener("change", () => {
 groupEventForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("teamLeaderName", groupEventForm.teamLeaderName.value);
-    formData.append("playerNames", groupEventForm.playerNames.value);
-    formData.append("email", groupEventForm.email.value);
-    formData.append("year", groupEventForm.year.value);
-    formData.append("branch", groupEventForm.branch.value);
+    const formData = new FormData(groupEventForm);
     formData.append("event", eventFieldGroup.value); 
-    formData.append("registrationNumber", groupEventForm.registrationNumber.value);
-    formData.append("transactionId", groupEventForm.transactionId.value);
 
     let fr = new FileReader();
     fr.onloadend = () => {
-        let base64Data = fr.result.split("base64,")[1];
+        const base64Data = fr.result.split("base64,")[1];
         formData.append("base64", base64Data);
         formData.append("imageType", fileInput.files[0].type);
         formData.append("imageName", fileInput.files[0].name);
 
-        fetch(groupEventForm.action, { method: 'POST', body: formData, mode: "no-cors" })
-        .then(() => {
-            alert("Thank you! Your group registration details are submitted.");
+        fetch(groupEventForm.action, { method: 'POST', body: formData })
+        .then(response => response.json())
+        .then(response => {
+            alert(response.message);
             groupEventForm.reset();
             preview.style.display = "none";
         })
