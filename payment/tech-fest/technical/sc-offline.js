aff9a4ce04e51aca0633986375347e3f0cbf3dde
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-24 00:34:52 +0530

    Update sc-offline.js

--- payment/tech-fest/technical/sc-offline.js
+++ payment/tech-fest/technical/sc-offline.js
@@ -1,4 +1,4 @@
-const offlineScriptURL = 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_URL/exec';
+const offlineScriptURL = 'https://script.google.com/macros/s/AKfycbwjbUi7USceSt1ksKNW-q2b9LSUL3RT93fEmXX_E6r_5k-lIB2q0zxaDe3W7wUrQb_J/exec';
 const offlineForm = document.forms['offlinePaymentForm'];
 
 const urlParams = new URLSearchParams(window.location.search);
