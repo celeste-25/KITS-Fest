91a6c619cdf35db3b62690d5eca3748bc1f76922
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-02-24 07:31:49 +0530

    Create mp-online.js

---
+++ payment/tech-fest/technical/mp-online.js
@@ -0,0 +1,54 @@
+const onlineScriptURL = 'https://script.google.com/macros/s/AKfycbxjmtzDA5QjZr89HQXRbii2VS8nrvmy7sa2EiGGJk4tBEOm6NNrwO2ZH8zBSUaKi2iu/exec';
+const onlineForm = document.forms['onlinePaymentForm'];
+
+const urlParams = new URLSearchParams(window.location.search);
+const eventName = urlParams.get("event");
+const eventField = document.getElementById("event");
+
+if (eventName) {
+    eventField.value = eventName;
+    document.getElementById('eventTitle').innerText = `Event: ${eventName}`;
+}
+
+const fileInput = document.getElementById('image');
+const preview = document.getElementById('preview');
+
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        preview.src = reader.result;
+        preview.style.display = "block";
+    };
+    reader.readAsDataURL(file);
+});
+
+onlineForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(onlineForm);
+    formData.append('event', eventField.value);
+
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        const base64String = reader.result.split(',')[1];
+        formData.append('base64', base64String);
+        formData.append('imageType', file.type);
+        formData.append('imageName', file.name);
+
+        fetch(onlineScriptURL, { method: 'POST', body: formData })
+            .then(response => response.json())
+            .then(response => {
+                if (response.result === 'success') {
+                    alert(response.message);
+                    onlineForm.reset();
+                    preview.style.display = "none";
+                } else {
+                    alert(response.message);
+                }
+            })
+            .catch(error => console.error('Error!', error.message));
+    };
+    reader.readAsDataURL(file);
+});
